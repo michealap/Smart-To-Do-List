b4b48fe291e22aa3ff4d86472c5cf087d46ff661
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-09 16:20:38 +0000

    add hard-code database

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -4,5 +4,13 @@ DROP TABLE IF EXISTS widgets CASCADE;
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  category VARCHAR(20) NOT NULL,
+  item VARCHAR(225) NOT NULL     --add new item to this column only if user_id exists.
+);
+
+DROP TABLE IF EXISTS lists CASCADE;
+CREATE TABLE lists (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  widget_id INTEGER REFERENCES widgets(id)
 );
--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -1,4 +1,2 @@
 -- Widgets table seeds here (Example)
-INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
-INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
-INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+INSERT INTO widgets ('category','item') VALUES ('Food'||'Books'||'Films'||'Products'||'Others', 1);
