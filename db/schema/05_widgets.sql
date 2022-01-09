019c8e1dd542cbbade9fb3a7fce409c178adc83c
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-09 18:29:30 +0000

    database

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -3,14 +3,13 @@
 DROP TABLE IF EXISTS widgets CASCADE;
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
   category VARCHAR(20) NOT NULL,
-  item VARCHAR(225) NOT NULL     --add new item to this column only if user_id exists.
+  item VARCHAR(225) NOT NULL     
 );
 
 DROP TABLE IF EXISTS lists CASCADE;
 CREATE TABLE lists (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  widget_id INTEGER REFERENCES widgets(id)
+  user_id INTEGER REFERENCES users(id) CASCADE ON DELETE,
+  widget_id INTEGER REFERENCES widgets(id) CASCADE ON DELETE
 );
--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -1,2 +1,20 @@
 -- Widgets table seeds here (Example)
-INSERT INTO widgets ('category','item') VALUES ('Food'||'Books'||'Films'||'Products'||'Others', 1);
+INSERT INTO widgets (category,item) VALUES ('Restaurant', 'Salad'),
+('Book', 'Don Quixote'),
+('Film', 'Encanto'),
+('Product','face cream'),
+('Restaurant', 'Pizza'),
+('Book', 'Don Quixote'),
+('Film', 'The wasteland'),
+('Product','watch');
+
+--lists bridge table
+
+INSERT INTO lists (user_id, widget_id) VALUES (1,1),
+(1,2),
+(1,3),
+(1,4),
+(2,5),
+(2,6),
+(2,7),
+(2,8);
