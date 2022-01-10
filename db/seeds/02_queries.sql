c498e075addebef6e588f3779169af8f9bf47211
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-10 22:46:37 +0000

    delete lists table and rename queries table

---
+++ db/seeds/02_queries.sql
@@ -0,0 +1,11 @@
+-- Widgets table seeds here (Example)
+INSERT INTO queries (category,item, user_id) VALUES ('Restaurant', 'Salad', 1),
+('Book', 'Don Quixote', 1),
+('Film', 'Encanto', 1),
+('Product','face cream', 1),
+('Restaurant', 'Pizza', 2),
+('Book', 'Don Quixote', 2),
+('Film', 'The wasteland', 2),
+('Product','watch', 2);
+
+
