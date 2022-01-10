5ba7c776046a3e95de75be934686a00bc36f501f
matthew woods <matthew.woods0157@gmail.com>
2022-01-10 14:33:22 +0000

    adding the .DS_Store file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,37 @@
 // Client facing scripts here
+const getAllQueries = function(user_id) {
+  const booksArray = [];
+  const filmArray = [];
+  const restaurantArray = [];
+  const productsArray = [];
+  const otherArray = [];
+
+  return db.query(`
+  SELECT widgets.category AS category, widgets.item AS item
+  FROM users
+  JOIN lists ON lists.user_id = users.id
+  JOIN widgets ON widgets.id = lists.query_id
+  WHERE users.id= ${user_id}
+  LIMIT 5`)
+  .then(res => {
+  res.rows.forEach(query => {
+    if(widgets.category === 'books') {
+      booksArray.push(query);
+    } else if (widgets.category === 'film') {
+      filmArray.push(query);
+    } else if (widgets.category === 'restaurant') {
+      restaurantArray.push(query);
+    } else if (widgets.category === 'products') {
+      productsArray.push(query);
+    } else {
+      otherArray.push(query);
+    }
+  });
+  })
+  .catch(err => console.error('query error', err.stack));
+}
+
+
+
+
+
