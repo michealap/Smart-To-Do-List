c119630c104bf861f7a8ab2fe7605c10e08d2451
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-11 14:59:29 +0000

    new file- functions to make query

--- routes/database.js
+++ routes/database.js
@@ -1,29 +1,10 @@
 const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
 
-const poolArray = [];
-const booksArray = [];
-const filmArray = [];
-const restaurantArray = [];
-const productsArray = [];
-const otherArray = [];
-pool.query(`
-SELECT widgets.category AS category, widgets.item AS item
-FROM users
-JOIN lists ON lists.user_id = users.id
-JOIN widgets ON widgets.id = lists.query_id
-WHERE users.id= ${users.id}`)
-.then(res => {
-  res.rows.forEach(query => {
-    if(widgets.category === 'books') {
-      booksArray.push(query);
-    } else if (widgets.category === 'film') {
-      filmArray.push(query);
-    } else if (widgets.category === 'restaurant') {
-      restaurantArray.push(query);
-    } else if (widgets.category === 'products') {
-      productsArray.push(query);
-    } else {
-      otherArray.push(query);
-    }
-  });
-})
+//enlist the items- grouping by the given category and user_id
+
+//add a new Item to the category with respect to the user_id
+
+//modify the category to the already added item: edit the category or delete the item
