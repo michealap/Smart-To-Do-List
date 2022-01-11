8becbe1f0ba979d6872454746c0219529ee65975
matthew woods <matthew.woods0157@gmail.com>
2022-01-11 00:33:05 +0000

    Adding another update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,13 @@
 // Client facing scripts here
 
 $(document).ready(function() {
-  $(#)
+  $('#search-form').submit(function(event) {
+    event.preventDefault();
+    let value = $(this).children('#search').val();
+    $.ajax({
+      url: '/apicalls',
+      type: 'POST',
+    })
+  })
 })
+
