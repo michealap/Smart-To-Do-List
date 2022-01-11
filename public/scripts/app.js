3aea84f9c2e0810d2e11ec13a752a97e4e521218
Micheala Palmer <michealapalmer@gmail.com>
2022-01-10 22:36:28 -0500

    setup event listeners

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,2 +1,26 @@
 // Client facing scripts here
-
+(function($) {
+  $(document).ready(() => {
+    $("#search").on('input', onInput);
+    $("#search-form").on('submit', onSubmit);
+   
+  });
+  const onInput = function(event) {
+    let $input = $('#search');
+    console.log("you have reached here:", $input);
+  };
+  // here are my named functions
+    
+  const onSubmit = function(event) {
+    event.preventDefault();
+    let $form = $(this);
+    let $input = $form.find('input');
+    console.log("you have submitted:", $input);
+  };
+  // let formData = $form.serialize();
+  // $.post("/requests", formData)
+  //   .then(function(response) {
+  //     console.log("inside promise");
+  //   });
+  //you can define a function outside of document.ready
+})(jQuery);
